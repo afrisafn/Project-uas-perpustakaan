dfb81765ae16b3faa2443a26cabef065a1f01f71
afrisafn <149044931+afrisafn@users.noreply.github.com>
2023-11-12 23:00:24 +0700

    Project uas perpustakaan sederhana

---
+++ UASPBO/src/uaspbo/Login_1.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uaspbo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author fauziah
+ */
+@Entity
+@Table(name = "login")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Login_1.findAll", query = "SELECT l FROM Login_1 l")
+    , @NamedQuery(name = "Login_1.findByUsername", query = "SELECT l FROM Login_1 l WHERE l.username = :username")
+    , @NamedQuery(name = "Login_1.findByPassword", query = "SELECT l FROM Login_1 l WHERE l.password = :password")})
+public class Login_1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    public Login_1() {
+    }
+
+    public Login_1(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Login_1)) {
+            return false;
+        }
+        Login_1 other = (Login_1) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "uaspbo.Login_1[ username=" + username + " ]";
+    }
+    
+}
---
+++ UASPBO/src/uaspbo/Skripsi_1.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uaspbo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author fauziah
+ */
+@Entity
+@Table(name = "skripsi")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Skripsi_1.findAll", query = "SELECT s FROM Skripsi_1 s")
+    , @NamedQuery(name = "Skripsi_1.findByIdSkripsi", query = "SELECT s FROM Skripsi_1 s WHERE s.idSkripsi = :idSkripsi")
+    , @NamedQuery(name = "Skripsi_1.findByJudulSkripsi", query = "SELECT s FROM Skripsi_1 s WHERE s.judulSkripsi = :judulSkripsi")
+    , @NamedQuery(name = "Skripsi_1.findByPenulisSkripsi", query = "SELECT s FROM Skripsi_1 s WHERE s.penulisSkripsi = :penulisSkripsi")
+    , @NamedQuery(name = "Skripsi_1.findByTahunSkripsi", query = "SELECT s FROM Skripsi_1 s WHERE s.tahunSkripsi = :tahunSkripsi")
+    , @NamedQuery(name = "Skripsi_1.findByHalamanSkripsi", query = "SELECT s FROM Skripsi_1 s WHERE s.halamanSkripsi = :halamanSkripsi")})
+public class Skripsi_1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_skripsi")
+    private Integer idSkripsi;
+    @Column(name = "judul_skripsi")
+    private String judulSkripsi;
+    @Column(name = "penulis_skripsi")
+    private String penulisSkripsi;
+    @Column(name = "tahun_skripsi")
+    private Integer tahunSkripsi;
+    @Column(name = "halaman_skripsi")
+    private String halamanSkripsi;
+
+    public Skripsi_1() {
+    }
+
+    public Skripsi_1(Integer idSkripsi) {
+        this.idSkripsi = idSkripsi;
+    }
+
+    public Integer getIdSkripsi() {
+        return idSkripsi;
+    }
+
+    public void setIdSkripsi(Integer idSkripsi) {
+        this.idSkripsi = idSkripsi;
+    }
+
+    public String getJudulSkripsi() {
+        return judulSkripsi;
+    }
+
+    public void setJudulSkripsi(String judulSkripsi) {
+        this.judulSkripsi = judulSkripsi;
+    }
+
+    public String getPenulisSkripsi() {
+        return penulisSkripsi;
+    }
+
+    public void setPenulisSkripsi(String penulisSkripsi) {
+        this.penulisSkripsi = penulisSkripsi;
+    }
+
+    public Integer getTahunSkripsi() {
+        return tahunSkripsi;
+    }
+
+    public void setTahunSkripsi(Integer tahunSkripsi) {
+        this.tahunSkripsi = tahunSkripsi;
+    }
+
+    public String getHalamanSkripsi() {
+        return halamanSkripsi;
+    }
+
+    public void setHalamanSkripsi(String halamanSkripsi) {
+        this.halamanSkripsi = halamanSkripsi;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idSkripsi != null ? idSkripsi.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Skripsi_1)) {
+            return false;
+        }
+        Skripsi_1 other = (Skripsi_1) object;
+        if ((this.idSkripsi == null && other.idSkripsi != null) || (this.idSkripsi != null && !this.idSkripsi.equals(other.idSkripsi))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "uaspbo.Skripsi_1[ idSkripsi=" + idSkripsi + " ]";
+    }
+    
+}
